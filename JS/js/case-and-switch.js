6a9e1c1e7a59af9043725d391b1f07e2f26e251b
Dley <wesdleyslv4@gmail.com>
2025-01-04 14:39:51 -0300

    creating a timer from javascript - aula

---
+++ JS/js/case-and-switch.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var nome = "wesdley"
+    switch(nome){
+        case "joao": //não esquecer de por os 2 pontos
+            alert('joao'); //poso fazer a ação que eu queriser
+        break;
+        case "guilherme":
+            alert("guilherme");
+        break;
+    }
+}
