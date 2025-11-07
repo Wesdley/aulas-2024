8b753d9a50521d714894a1be9e17b533f87c8d00
Dley <wesdleyslv4@gmail.com>
2025-11-07 14:56:13 -0300

    funções II

---
+++ Aulas JavaScript/06 - Funções II/JavaScript.js
@@ -0,0 +1,14 @@
+var func = function () { // pode por const ou let!!
+    alert('ola mundo')
+}
+
+func();
+
+// diferente, mas da para utilizar
+
+(function () {
+    console.log('ola mundo')
+})(); // nessa parte a chamamos
+
+
+
