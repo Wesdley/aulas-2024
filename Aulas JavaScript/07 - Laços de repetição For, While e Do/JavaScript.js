583570519dd949fb6000ce2cb0f2125f316a3268
Dley <wesdleyslv4@gmail.com>
2025-11-07 15:02:31 -0300

    Laços de repetição

---
+++ Aulas JavaScript/07 - Laços de repetição For, While e Do/JavaScript.js
@@ -0,0 +1,15 @@
+var n = 0
+
+/*while (n < 10) { // enquanto for menor vai executar
+    console.log(n);
+    n++;
+}*/
+
+/*do {
+    console.log(n);
+    n++;
+} while (n < 10) // faz a verificação depois de executar */
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
